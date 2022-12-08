4168de2d3a878a8029a987357584eb1cf75f0373
waterbinnn <ewaterbin@naver.com>
2022-12-09 03:48:29 +0900

    💄 Add secondary color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 export const colors = {
-    black: '#000000',
-    white: '#ffffff',
-    primary: '#FFEB83',
-    error:'#FF2525' 
-  };
-  
+  black: '#000000',
+  white: '#ffffff',
+  primary: '#FFEB83',
+  error: '#FF2525',
+  secondary: '#5999ff',
+};
