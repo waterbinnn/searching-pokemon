8378eed71e37e2f7a11181258685bc0ed1dc26a7
waterbinnn <ewaterbin@naver.com>
2022-12-08 19:33:24 +0900

    🎉 Project Initial setting

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # freed-test
+
+### Stack
+
+TypeScript, React, Redux-toolkit, Styled-component
+
+### 설치
+
+```
+npm i --save-dev @types/styled-components
+npm i styled-reset
+npm i axios
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Pokemon from './components/Pokemon';
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <Pokemon/>
+    </>
+  );
+}
+
+export default App;
