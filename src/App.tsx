de1947abbc40084d89aa29b0ed44c8e6e2610d57
waterbinnn <ewaterbin@naver.com>
2022-12-09 03:03:01 +0900

    ♻️ emtion style로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
 import Header from './components/Header';
 import Pokemon from './components/Pokemon';
+
+import { container } from './styles/Global.style';
 
 function App() {
   return (
     <>
-     <Header/>
-     <Pokemon/>
+      <div css={container}>
+        <Header />
+        <Pokemon />
+      </div>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './styles/Global.style';
+import { Global } from '@emotion/react';
+import { globalStyle } from './styles/Global.style';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>
 );
-
