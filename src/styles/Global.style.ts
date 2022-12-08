4145a6b01d0bee40c2c118fce6377ee28c90d65a
waterbinnn <ewaterbin@naver.com>
2022-12-09 03:00:17 +0900

    💄 styled-> emotion 으로 변경, hidden, 레이아웃 스타일 작성

--- src/styles/Global.style.ts
+++ src/styles/Global.style.ts
@@ -1,21 +1,19 @@
-import { createGlobalStyle } from 'styled-components';
-import { reset } from 'styled-reset';
+import { css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 import { colors } from './theme';
 
-export const GlobalStyle = createGlobalStyle`
-  ${reset}
-
+export const globalStyle = css`
+  ${emotionReset},
   *,
   *:before,
   *:after {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
   }
-  
+
   body {
-    width:100%;
+    width: 100%;
     height: 100vh;
     font-family: 'Noto Sans KR', sans-serif;
     color: ${colors.black};
@@ -46,11 +44,29 @@ export const GlobalStyle = createGlobalStyle`
     background-color: transparent;
     cursor: pointer;
     padding: 0;
-    margin: 0; 
+    margin: 0;
     border: none;
   }
 
-  svg, image, video {
+  svg,
+  image,
+  video {
     vertical-align: top;
   }
+
+  .visually-hidden {
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+  }
+`;
+
+export const container = css`
+  margin: 0 10px;
 `;
