9e7afb26c4736e14fdb672de0c804889b941a30e
waterbinnn <ewaterbin@naver.com>
2022-12-09 14:20:06 +0900

    ➕ Add Provider, store for using redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Global } from '@emotion/react';
 import { globalStyle } from './styles/Global.style';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Global styles={globalStyle} />
-    <App />
+    <Provider store={store}>
+      <Global styles={globalStyle} />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
