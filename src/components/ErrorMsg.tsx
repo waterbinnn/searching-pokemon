540faea29f97ceb296d245b9b4b7edb3bad792f9
waterbinnn <ewaterbin@naver.com>
2022-12-09 18:27:30 +0900

    🔨결과 없을 때 4초간 searching -> error 화면 보이도록 구현

--- src/components/ErrorMsg.tsx
+++ src/components/ErrorMsg.tsx
@@ -1,14 +1,37 @@
 /** @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react';
 import { errorMsg } from '../styles/components/State.style';
+import Searching from './Searching';
 
 const ErrorMsg = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    if (count === 4) {
+      clearInterval(timer);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [count]);
+
   return (
-    <div css={errorMsg}>
-      <img src={require('../assets/error.png')} alt="" />
-      <strong>ERROR!</strong>
-      <p>Sorry man</p>
-      <p>You sure you got the right input?</p>
-    </div>
+    <>
+      {count === 4 ? (
+        <div css={errorMsg}>
+          <img src={require('../assets/error.png')} alt="" />
+          <strong>ERROR!</strong>
+          <p>Sorry man</p>
+          <p>You sure you got the right input?</p>
+        </div>
+      ) : (
+        <Searching />
+      )}
+    </>
   );
 };
 
