575a77f708fc316ca3369b73a1cbab518f01f0dc
waterbinnn <ewaterbin@naver.com>
2022-12-09 16:23:26 +0900

    🚧 입력한 id, name에 맞는 데이터 화면에 구현

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -8,41 +8,52 @@ import {
   pokeContainer,
   pokeInfo,
 } from '../styles/components/Pokemon.style';
+import { useSelector } from 'react-redux';
+import { getData } from '../redux/pokeSlice';
+import ErrorMsg from './ErrorMsg';
 
 const PokemonCard = () => {
+  const data = useSelector(getData);
+
   return (
-    <section css={pokeContainer}>
-      <h2 className="visually-hidden">Searched Pokemon</h2>
-      <div css={pokeWrapper}>
-        <strong css={pokeId}>#4</strong>
-        <strong css={pokeName}>name</strong>
-        <img
-          css={pokeImg}
-          src="https://cdn.cnn.com/cnnnext/dam/assets/210226041521-03-pokemon-anniversary-design-full-169.jpg"
-          alt=""
-        />
-        <table css={pokeInfo}>
-          <tbody>
-            <tr>
-              <th>Type :</th>
-              <td>Fire</td>
-            </tr>
-            <tr>
-              <th>HP :</th>
-              <td>44</td>
-            </tr>
-            <tr>
-              <th>Attack :</th>
-              <td>28</td>
-            </tr>
-            <tr>
-              <th>Deffense :</th>
-              <td>65</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
+    <>
+      {!data ? (
+        <ErrorMsg />
+      ) : (
+        <section css={pokeContainer}>
+          <h2 className="visually-hidden">Searched Pokemon</h2>
+          <div css={pokeWrapper}>
+            <strong css={pokeId}>#{data.id}</strong>
+            <strong css={pokeName}>{data.name}</strong>
+            <img
+              css={pokeImg}
+              src={data.sprites['front_default']}
+              alt={data.name}
+            />
+            <table css={pokeInfo}>
+              <tbody>
+                <tr>
+                  <th>Type :</th>
+                  <td>{data.types[0].type.name}</td>
+                </tr>
+                <tr>
+                  <th>HP :</th>
+                  <td>{data.stats[0].base_stat}</td>
+                </tr>
+                <tr>
+                  <th>Attack :</th>
+                  <td>{data.stats[1].base_stat}</td>
+                </tr>
+                <tr>
+                  <th>Deffense :</th>
+                  <td>{data.stats[2].base_stat}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
