28ccf9b73bdd6a9d47125f81eb93db8cabf48976
waterbinnn <ewaterbin@naver.com>
2022-12-09 14:19:13 +0900

    🔨 검색 id, name 에 따라 data 불러오도록 구현

--- src/components/SearchFrom.tsx
+++ src/components/SearchFrom.tsx
@@ -1,15 +1,37 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../redux/store';
+import { fetchPokemon } from '../redux/pokeSlice';
 import {
   formContainer,
   inputWrapper,
 } from '../styles/components/SearchForm.style';
 
 const SearchForm = () => {
+  const [inputValue, setInputValue] = useState('');
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    dispatch(fetchPokemon(inputValue));
+    setInputValue('');
+  };
+
   return (
-    <form css={formContainer}>
+    <form css={formContainer} onSubmit={handleSubmit}>
       <label htmlFor="pokemon">Poke-Id or Name</label>
       <div css={inputWrapper}>
-        <input type="text" id="pokemon" />
+        <input
+          type="text"
+          id="pokemon"
+          value={inputValue}
+          onChange={handleInput}
+        />
         <button type="submit">🔍</button>
       </div>
     </form>
