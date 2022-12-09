05bdf1c9c5225e48972f8a89e9da30efa8ccad74
waterbinnn <ewaterbin@naver.com>
2022-12-09 17:18:07 +0900

    🔨 비동기 상태 제어

--- src/redux/pokeSlice.tsx
+++ src/redux/pokeSlice.tsx
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { AsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const pokeApi = 'https://pokeapi.co/api/v2/pokemon/';
@@ -6,29 +7,37 @@ const pokeApi = 'https://pokeapi.co/api/v2/pokemon/';
 export const fetchPokemon = createAsyncThunk(
   'pokemon/fetchPokemon',
   async (term: string | number) => {
-    const response = await axios.get(pokeApi + term);
-    console.log(response.data);
-    return response.data;
+    try {
+      const response = await axios.get(pokeApi + term);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 );
 
 const initialState = {
   term: '',
+  isLoading: false,
 };
 
 const pokeSlice = createSlice({
   name: 'pokemon',
   initialState,
-  reducers: {
-    searchTerm: (state, action) => {
-      state.term = action.payload;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchPokemon.fulfilled, (state, action) => {
-      state.term = action.payload;
+    builder.addCase(fetchPokemon.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchPokemon.fulfilled, (state, { payload }) => {
+      return { ...state, pokemon: payload, isLoading: false };
+    });
+    builder.addCase(fetchPokemon.rejected, (state) => {
+      state.isLoading = false;
     });
   },
 });
+
+export const getData = (state: any) => state.pokemon.pokemon;
 
 export default pokeSlice.reducer;
